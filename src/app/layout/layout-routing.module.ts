ef3ac55e0fde2fd5f177cdde8db81809fcb33cae
Ng Wei Xiang <weixng97@gmail.com>
2024-08-07 01:13:25 +0800

    commit for menu

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -3,7 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout.component';
 
 const routes: Routes = [
-  {path: '', component: LayoutComponent}
+  {path: '', component: LayoutComponent, children: [
+    {path: '', redirectTo: 'menu', pathMatch: 'prefix'},
+    {path: 'menu', loadChildren: () => import('./menu/menu.module').then(m=>m.MenuModule)}
+  ]}
 ];
 
 @NgModule({
